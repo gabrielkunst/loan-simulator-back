ea93ead3c2694752abf8e5bb7ad7dceaac42914f
Gabriel Kunst <gabrielkt.dev@gmail.com>
2024-08-03 10:44:33 -0300

    refactor: create prisma singleton to avoid having more than one instance

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,20 @@
 import { env } from '@/env'
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient({
-  log: env.NODE_ENV === 'dev' ? ['query'] : [],
-})
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: env.NODE_ENV === 'dev' ? ['query'] : [],
+  })
+}
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+if (env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma
+}
+
+export { prisma }
